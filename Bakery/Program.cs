4be5d9422d40301f92d2a8a5e388aedc6b223d2d
Garrett Hays <GarrettlHays@gmail.com>
2022-07-15 15:09:38 -0700

    adds further exploration, including user storypathing.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -20,15 +20,32 @@ public class Program
     int breadCount = newBreadCount.GetBreadCount(userInputBread);
     int breadTotal = newBreadCount.BreadCalculator(breadCount);
     breadTotal = newBreadCount.BreadDealCalculator(breadTotal, breadCount);
-
+    Console.WriteLine("'What kind of bread would you like? Rye or Wheat?");
+    string userInputBreadType = Console.ReadLine();
+    userInputBreadType = userInputBreadType.ToLower();
+    if (userInputBreadType != "rye" && userInputBreadType !="wheat")
+    {
+      Console.WriteLine("'No bread for you then.' Pierre says");
+      breadTotal = 0;
+    }
     Console.WriteLine("'And how many pastries would you like?' Pierre asks");
     string userInputPastry = Console.ReadLine();
     PastryClass newPastryCount = new PastryClass();
     int pastryCount = newPastryCount.GetPastryCount(userInputPastry);
     int pastryTotal = newPastryCount.PastryCalculator(pastryCount);
     pastryTotal = newPastryCount.PastryDealCalculator(pastryTotal, pastryCount);
-
-    Console.WriteLine("'Fantastique! Your total will be: $" + (breadTotal + pastryTotal) + ".'");
+    Console.WriteLine("'What kind of pastry would you like? Raspberry or Chocolate?");
+    string userInputPastryType = Console.ReadLine();
+    userInputPastryType =  userInputPastryType.ToLower();
+    if ((userInputPastryType != "raspberry" && userInputPastryType !="chocolate") && breadTotal == 0)
+    {
+      Console.WriteLine("'Get out of my shop.' Pierre says");
+      return;
+    } else if (userInputPastryType != "raspberry" && userInputPastryType !="chocolate") {
+      Console.WriteLine("'No pastries for you then.' Pierre says");
+      pastryTotal = 0;
+    }
+    Console.WriteLine($"'Fantastique! Your total will be: ${breadTotal + pastryTotal} for {breadCount} loaves of {userInputBreadType} bread and {pastryCount} {userInputPastryType} pastries.'");
     Console.WriteLine("You hand Pierre $" + (breadTotal + pastryTotal + 5) + ".");
     Console.WriteLine("'I don't have any change, so thanks for the tip!' Pierre Says.");
   }
