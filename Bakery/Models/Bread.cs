57fdfa5ce979c5c4b5645fbcaae5aa345a70dd47
Garrett Hays <GarrettlHays@gmail.com>
2022-07-15 10:55:08 -0700

    adds code to pass bread calculation test - green phase

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -3,11 +3,22 @@ namespace BreadModels
   public class BreadClass
   {
     public int BreadCount {get; set;}
+    public int BreadSubTotal {get; set;}
 
     public int Bread(string count)
     {
       BreadCount = int.Parse(count);
+      BreadSubTotal = 0;
       return BreadCount;
+    }
+
+    public int BreadCalculator()
+    {
+      for (int i = 0; i < BreadCount; i++)
+      {
+        BreadSubTotal += 5;
+      }
+      return BreadSubTotal;
     }
   }
 }
