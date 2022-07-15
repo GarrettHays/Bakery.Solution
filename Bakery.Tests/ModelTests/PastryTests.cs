51b2ef2bdacd2babd10a18ad35c8f575b2b87be4
Garrett Hays <GarrettlHays@gmail.com>
2022-07-15 14:15:53 -0700

    updated test to reflect the refactored code in Pastry.cs

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -12,67 +12,32 @@ namespace PastryTests
     public void ItemConstructor_CreatePastryObject_CustomerPastry()
     {
       PastryClass newCustomer = new PastryClass();
-      newCustomer.Pastry("1");
+      newCustomer.GetPastryCount("1");
       Assert.AreEqual(typeof(PastryClass), newCustomer.GetType());
     }
     [TestMethod]
     public void ItemConstructor_CorrectlyParseString_int()
     {
       PastryClass newCustomer = new PastryClass();
-      newCustomer.Pastry("1");
-      Assert.AreEqual(1, newCustomer.PastryCount);
+      int pastryCount = newCustomer.GetPastryCount("1");
+      Assert.AreEqual(1, pastryCount);
     }
     [TestMethod]
     public void AddPastry_AddPastryPrices_Int()
     {
       PastryClass newCustomer = new PastryClass();
-      newCustomer.Pastry("1");
-      newCustomer.PastryCalculator();
-      Assert.AreEqual(2, newCustomer.PastrySubTotal);
+      int pastryCount = newCustomer.GetPastryCount("1");
+      int pastryTotal = newCustomer.PastryCalculator(pastryCount);
+      Assert.AreEqual(2, pastryTotal);
     }
     [TestMethod]
     public void AddPastry_CorrectlyCalculateDealPrice_Int()
     {
       PastryClass newCustomer = new PastryClass();
-      newCustomer.Pastry("3");
-      newCustomer.PastryCalculator();
-      newCustomer.PastryDealCalculator();
-      Assert.AreEqual(5, newCustomer.PastrySubTotal);
+      int pastryCount = newCustomer.GetPastryCount("3");
+      int pastryTotal = newCustomer.PastryCalculator(pastryCount);
+      pastryTotal = newCustomer.PastryDealCalculator(pastryTotal, pastryCount);
+      Assert.AreEqual(5, pastryTotal);
     }
   }
 } 
-//     [TestMethod]
-// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
-// {
-//   // any necessary logic to prep for test; instantiating new classes, etc.
-//   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
-  //}
-    // [TestMethod]
-    // public void ItemConstructor_CreatesInstanceOfItem_Item()
-    // {
-    //   Item newItem = new Item();
-    //   Assert.AreEqual(typeof(Item), newItem.GetType());
-    // }
-
-//     [TestMethod]
-//     public void GetDescription_ReturnsDescription_String()
-//     {
-//       string description = "Walk the dog.";
-//       Item newItem = new Item(description);
-//       string result = newItem.Description;
-//       Assert.AreEqual(description, result);
-//     }
-
-//     [TestMethod]
-// public void GetDescription_ReturnsDescription_String()
-// {
-//   //Arrange
-//   string description = "Walk the dog.";
-//   Item newItem = new Item(description);
-
-//   //Act
-//   string result = newItem.Description;
-
-//   //Assert
-//   Assert.AreEqual(description, result);
-// }
