9da07ebdd1acef83f42bd4c5b88af48cddb2151f
Garrett Hays <GarrettlHays@gmail.com>
2022-07-15 11:11:28 -0700

    adds code to pass pastry calculation test - green phase

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -3,11 +3,22 @@ namespace PastryModels
   public class PastryClass
   {
     public int PastryCount {get; set;}
+    public int PastrySubTotal {get; set;}
 
     public int Pastry(string count)
     {
       PastryCount = int.Parse(count);
+      PastrySubTotal = 0;
       return PastryCount;
+    }
+
+    public int PastryCalculator()
+    {
+      for (int i = 0; i < PastryCount; i++)
+      {
+        PastrySubTotal += 2;
+      }
+      return PastrySubTotal;
     }
   }
 }
