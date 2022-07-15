3edc98711f7638ccb75015b3ff8338a63afd57e3
Garrett Hays <GarrettlHays@gmail.com>
2022-07-15 09:42:46 -0700

    adds test to instantiate a bread object -red phase

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,54 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Bread.Models;
+
+namespace Bread.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void ItemConstructor_CreateBreadObject_CustomerBread()
+    {
+      Bread newCustomer = new Bread("4");
+      Assert.AreEqual(typeof(Bread), newCust.GetType());
+    }
+  }
+} 
+
+//     [TestMethod]
+// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+// {
+//   // any necessary logic to prep for test; instantiating new classes, etc.
+//   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+  //}
+    // [TestMethod]
+    // public void ItemConstructor_CreatesInstanceOfItem_Item()
+    // {
+    //   Item newItem = new Item();
+    //   Assert.AreEqual(typeof(Item), newItem.GetType());
+    // }
+
+//     [TestMethod]
+//     public void GetDescription_ReturnsDescription_String()
+//     {
+//       string description = "Walk the dog.";
+//       Item newItem = new Item(description);
+//       string result = newItem.Description;
+//       Assert.AreEqual(description, result);
+//     }
+
+//     [TestMethod]
+// public void GetDescription_ReturnsDescription_String()
+// {
+//   //Arrange
+//   string description = "Walk the dog.";
+//   Item newItem = new Item(description);
+
+//   //Act
+//   string result = newItem.Description;
+
+//   //Assert
+//   Assert.AreEqual(description, result);
+// }
